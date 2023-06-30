354b7468f9fb2624676a4a16915d755d73018f4f
Aaron Sala <salaaron2@gmail.com>
2023-06-30 10:29:04 +0300

    added the database part in the admin

---
+++ public/script.js
@@ -0,0 +1,177 @@
+// Get the form element
+const carForm = document.getElementById('carForm');
+
+// Add event listener for form submission
+carForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form from submitting and page refresh
+
+  // Get the form data
+  const formData = new FormData(carForm);
+
+  // Create an object to hold the car data
+  const carData = {
+    maker: formData.get('maker'),
+    model: formData.get('model'),
+    year: formData.get('year'),
+    price: formData.get('price'),
+    image: formData.get('image'),
+    mileage: formData.get('mileage'),
+    shape: formData.get('shape'),
+    category: formData.get('category'),
+    description: formData.get('description'),
+    engine: formData.get('engine')
+  };
+
+  // Get the image file from the form data
+  const imageFile = formData.get('image');
+
+  // Send a POST request to the server to save the car data
+  axios.post('/cars', carData)
+    .then(function(response) {
+      console.log('Car added:', response.data);
+      carForm.reset(); // Reset the form
+      fetchAndDisplayCars(); // Fetch and display the updated car data
+    })
+    .catch(function(error) {
+      console.error('Error adding car:', error);
+    });
+});
+
+// Function to fetch and display the car data
+function fetchAndDisplayCars() {
+  // Send a GET request to the server to retrieve the car data
+  axios.get('/cars')
+    .then(function(response) {
+      const carList = response.data;
+
+      // Clear previous car list
+      const carListElement = document.getElementById('carList');
+      carListElement.innerHTML = '';
+
+      // Iterate over the car list and create HTML elements to display each car
+      carList.forEach(function(car) {
+        const carElement = document.createElement('div');
+        carElement.classList.add('car');
+
+        const carImageElement = document.createElement('img');
+        carImageElement.src = `/uploads/${car.image}`;
+        carElement.appendChild(carImageElement);
+
+        const carInfoElement = document.createElement('div');
+        carInfoElement.innerHTML = `
+          <h2>${car.maker} ${car.model}</h2>
+          <p>Year: ${car.year}</p>
+          <p>Price: ${car.price}</p>
+          <p>mileage: ${car.mileage}</p>
+          <p>engine: ${car.engine}</p>
+          <p>description: ${car.description}</p>
+          <p>shape: ${car.shape}</p>
+          <p>category: ${car.category}</p>
+
+        `;
+        carElement.appendChild(carInfoElement);
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function() {
+          openEditForm(car._id, car.maker, car.model, car.year, car.price);
+        });
+        carElement.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function() {
+          deleteCar(car._id);
+        });
+        carElement.appendChild(deleteButton);
+
+        carListElement.appendChild(carElement);
+      });
+    })
+    .catch(function(error) {
+      console.error('Error fetching cars:', error);
+    });
+}
+
+// Call the fetchAndDisplayCars function to load and display the initial car data
+fetchAndDisplayCars();
+
+// Function to open the edit form
+function openEditForm(carId, maker, model, year, price) {
+  // Populate the form fields with existing car data
+  document.getElementById('editCarId').value = carId;
+  document.getElementById('editMaker').value = maker;
+  document.getElementById('editModel').value = model;
+  document.getElementById('editYear').value = year;
+  document.getElementById('editPrice').value = price;
+  document.getElementById('editShape').value = price;
+  document.getElementById('editEngine').value = price;
+  document.getElementById('editCategory').value = price;
+  document.getElementById('editDescription').value = price;
+
+  // Show the edit form
+  document.getElementById('editFormContainer').style.display = 'block';
+}
+
+// Function to handle form submission for updating a car
+document.getElementById('editCarForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form from submitting and page refresh
+
+  // Get the form data
+  const formData = new FormData(event.target);
+
+  // Create an object to hold the car data
+  const carData = {
+    maker: formData.get('editMaker'),
+    model: formData.get('editModel'),
+    year: formData.get('editYear'),
+    price: formData.get('editPrice'),
+    shape: formData.get('editShape'),
+    description: formData.get('editDescription'),
+    engine: formData.get('editEngine'),
+    category: formData.get('editCategory')
+  };
+
+  const carId = formData.get('editCarId');
+
+  // Send a PUT request to the server to update the car data
+  axios.put(`/cars/${carId}`, carData)
+    .then(function(response) {
+      console.log('Car updated:', response.data);
+      closeEditForm(); // Close the edit form
+      fetchAndDisplayCars(); // Fetch and display the updated car data
+    })
+    .catch(function(error) {
+      console.error('Error updating car:', error);
+    });
+});
+
+// Function to close the edit form
+function closeEditForm() {
+  // Clear the form fields
+  document.getElementById('editCarId').value = '';
+  document.getElementById('editMaker').value = '';
+  document.getElementById('editModel').value = '';
+  document.getElementById('editYear').value = '';
+  document.getElementById('editPrice').value = '';
+  document.getElementById('editShape').value = '';
+  document.getElementById('editDescription').value = '';
+  document.getElementById('editEngine').value = '';
+  document.getElementById('editCategory').value = '';
+
+  // Hide the edit form
+  document.getElementById('editFormContainer').style.display = 'none';
+}
+
+// Function to delete a car
+function deleteCar(carId) {
+  // Send a DELETE request to the server to delete the car
+  axios.delete(`/cars/${carId}`)
+    .then(function(response) {
+      console.log('Car deleted:', response.data);
+      fetchAndDisplayCars(); // Fetch and display the updated car data
+    })
+    .catch(function(error) {
+      console.error('Error deleting car:', error);
+    });
+}
---
+++ server.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/denis', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB:', error));
+
+// Create a car schema
+const carSchema = new mongoose.Schema({
+  maker: String,
+  model: String,
+  year: Number,
+  image: String,
+  price: Number,
+  category: String,
+  shape: String,
+  description: String,
+  engine: String,
+  mileage: String,
+});
+
+// Create a car model
+const Car = mongoose.model('Car', carSchema);
+
+// Create a storage configuration for multer
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'public/uploads'); // Specify the destination folder for uploaded files
+  },
+  filename: function(req, file, cb) {
+    // Generate a unique filename for the uploaded file
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+// Create the multer middleware using the storage configuration
+const upload = multer({ storage: storage });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Serve the HTML file for the root path
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'admin.html'));
+});
+
+// Handle POST request for creating a new car
+app.post('/cars', upload.single('image'), function(req, res) {
+  const { maker, model, year, price, mileage, category, engine, description, shape } = req.body;
+  const imagePath = req.file ?'uploads'+ req.file.filename : ''; // Get the file name from the uploaded image
+
+  // Create a new car object
+  const car = new Car({
+    maker,
+    model,
+    year,
+    price,
+    mileage,
+    category,
+    engine,
+    description,
+    shape,
+    image: imagePath // Assign the file name to the image field
+  });
+
+  // Save the car object to the database
+  car.save()
+    .then(() => {
+      console.log('Car saved:', car);
+      res.redirect('/');
+    })
+    .catch(error => console.error('Error saving car:', error));
+});
+// Handle DELETE request for deleting a car
+app.delete('/cars/:id', function(req, res) {
+  const carId = req.params.id;
+
+  // Find the car by ID and delete it
+  Car.findByIdAndRemove(carId)
+    .then(() => {
+      console.log('Car deleted:', carId);
+      res.sendStatus(204); // Send a success status without content
+    })
+    .catch(error => {
+      console.error('Error deleting car:', error);
+      res.status(500).json({ error: 'Error deleting car' });
+    });
+});
+
+
+// Handle PUT request for updating a car
+app.put('/cars/:id', function(req, res) {
+  const carId = req.params.id;
+  const { maker, model, year, price, shape, mileage, description, engine, category } = req.body;
+
+  // Find the car by its ID and update the fields
+  Car.findByIdAndUpdate(carId, { maker, model, year, price,shape, mileage, description, engine, category  }, { new: true })
+    .then(updatedCar => {
+      if (updatedCar) {
+        console.log('Car updated:', updatedCar);
+        res.json(updatedCar);
+      } else {
+        console.error('Car not found');
+        res.status(404).json({ error: 'Car not found' });
+      }
+    })
+    .catch(error => {
+      console.error('Error updating car:', error);
+      res.status(500).json({ error: 'Error updating car' });
+    });
+});
+
+
+// ... existing code ...
+
+
+// Handle GET request for retrieving all cars
+app.get('/cars', function(req, res) {
+  Car.find()
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(error => {
+      console.error('Error fetching cars:', error);
+      res.status(500).json({ error: 'Error fetching cars' });
+    });
+});
+
+// Start the server
+app.listen(3001, function() {
+  console.log('Server listening on port 3001');
+});
