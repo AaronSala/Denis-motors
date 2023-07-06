a0c3792e03292f04a6a4adb468b77d3fc8677ccb
Aaron Sala <salaaron2@gmail.com>
2023-07-06 07:55:40 +0300

    added database and login form for admin

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const ObjectId = mongodb.ObjectId;
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Database connection URL
 const url = 'mongodb://localhost:27017';
@@ -43,8 +45,56 @@ app.get('/cars', async (req, res) => {
   }
 });
 
-// Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/denis', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Define User schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
 });
+
+const User = mongoose.model('User', userSchema, 'users');
+
+// Parse request body
+app.use(bodyParser.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle user login request
+app.post('/login', async(req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find a user with the provided username and password
+    const user = await User.findOne({ username, password });
+
+    if (user) {
+      
+      // User found, send success status
+      res.sendStatus(200);
+    } else {
+      // User not found, send unauthorized status
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    console.error('Error finding user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+// Start the server
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+// Start the server
